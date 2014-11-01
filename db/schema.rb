f34022a8621bb48d1ea6ec67eb6ae5c0c01d0373
jeroenstevens <jeroenstevens@live.nl>
2014-11-01 21:40:16 +0100

    Change user_id of collection from int to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101135349) do
+ActiveRecord::Schema.define(version: 20141101202402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20141101135349) do
   create_table "collections", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
     t.string   "name"
     t.string   "image_url"
-    t.integer  "user_id"
+    t.uuid     "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_file_name"
